a45dd392b6330c4a660b9e66a092adc45550bc78
Akshvir <akshvir.soni@gmail.com>
2017-09-23 20:57:02 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname +"/views/partials");
+app.set("view engine","hbs");
+app.use(express.static(__dirname + "/public"));
+
+app.use((req,res,next) =>
+{
+    var log = new Date().toString() + ` ${req.method} ${req.url}`;
+    fs.appendFile("server.log",log +"\n",(err) =>
+    {
+        if (err)
+            console.log("error");
+    });
+    next();
+})
+
+app.use((req,res,next) =>
+{
+    res.render("maintain.hbs",{
+        pageTitle:"Maintainance Page",
+        bodyContent:"This page is under maintainance."
+    });
+});
+
+hbs.registerHelper("currentYear",() =>
+{
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper("screamIt",(text) =>
+{
+    return text.toUpperCase();
+})
+
+app.get("/",(req,res) => {
+    res.render("home.hbs",{
+        pageTitle:"Home Page",
+        bodyContent:"This is the body of the page",
+    })
+});
+
+app.get("/about",(req,res) => {
+    res.render("about.hbs",{
+        pageTitle:"About Page",
+        //currentYear:new Date().getFullYear()
+    })
+});
+
+app.get("/bad",(req,res) => {
+    res.send({
+        Request:"BAD",
+        Status:"Nothing Loaded"
+    });
+});
+
+app.listen(3000);
